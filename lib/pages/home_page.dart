e77734d10e35336d76129bd9b38f24d5d6330f27
John Smith <a.harmel@esi-sba.dz>
2024-06-11 21:26:33 +0100

    the home page been updated added phone textFormField added password textFormField

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:form/utlis/global_Constant.dart';
 import 'package:form/widget/custom_form_field.dart';
 import 'package:form/utlis/input_validators.dart';
+import 'package:form/widget/custom_signup_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -16,53 +17,66 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const SizedBox(
-                height: 50,
-                child: Text("signUp ")),
-            Expanded(
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("signUp ",),
+              const SizedBox(height: 50,),
+              Expanded(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const SizedBox(height: 20,),
+                   CustomFormField(
+                     hint: "name",
+                     validator: (value) {
+                       if(value!.isNotEmpty && value!.isNameValid){
+                         return "enter a valid name";
+                       }
+                     },
+                     inputFormater: GlobalConstant.inputFormaterNameField,
+                   ),
+                   const SizedBox(height: 20,),
+                   CustomFormField(
+                     hint: "email",
+                     inputFormater: GlobalConstant.inputFormaterEmailField,
+                     validator: (value) {
+                       if(value!.isNotEmpty && value!.isEmailValid){
+                         return "enter a valid email";
+                       }
+                     },
+                   ),
                   const SizedBox(height: 20,),
-                 CustomFormField(
-                   hint: "name",
-                   validator: (value) {
-                     if(value!.isNotEmpty && value!.isNameValid){
-                       return "enter a valid name";
-                     }
-                   },
-                   inputFormater: GlobalConstant.inputFormaterNameField,
-                 ),
-                 const SizedBox(height: 20,),
-                 CustomFormField(
-                   hint: "email",
-                   inputFormater: GlobalConstant.inputFormaterEmailField,
-                   validator: (value) {
-                     if(value!.isNotEmpty && value!.isEmailValid){
-                       return "enter a valid email";
-                     }
-                   },
-                 ),
-                const SizedBox(height: 20,),
-                CustomFormField(
-                  hint: "telephone",
-                  inputFormater: GlobalConstant.inputFormaterPhoneField,
-                  validator: (value) {
-                    if(value!.isNotEmpty && value!.isTelephoneValid){
-                      return "enter a valid telephone";
-                    }
-                  },
+                  CustomFormField(
+                    hint: "telephone",
+                    inputFormater: GlobalConstant.inputFormaterPhoneField,
+                    validator: (value) {
+                      if(value!.isNotEmpty && value!.isTelephoneValid){
+                        return "enter a valid telephone";
+                      }
+                    },
+                  ),
+                    const SizedBox(height: 20,),
+                    CustomFormField(
+                      hint: "password",
+                      validator: (value) {
+                        if(value!.isNotEmpty && value!.isPasswordValid){
+                          return "enter a valid password";
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                ],
               ),
-            ),
-        
-        
-        
-        ],),
+              CustomSignupButton(
+                  buttonHandler:(){},
+                  buttonContent: "SignUp")
+
+
+          ],),
+        ),
       ),
     );
   }
