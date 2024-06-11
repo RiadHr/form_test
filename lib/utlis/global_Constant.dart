2cc92832265b7c5c63d8e73663bf5053f947e798
John Smith <a.harmel@esi-sba.dz>
2024-06-11 21:27:15 +0100

    adding style for signup button

--- lib/utlis/global_Constant.dart
+++ lib/utlis/global_Constant.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class GlobalConstant{
@@ -6,4 +7,11 @@ class GlobalConstant{
   static final inputFormaterNameField = [FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z]+|\s"))];
   static final inputFormaterPhoneField = [FilteringTextInputFormatter.allow(RegExp(r"[0-9]"))];
   static final inputFormaterEmailField = [FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z]+|\s"))];
+
+  static final kbuttonSignupStyle = ButtonStyle(
+    shape: MaterialStateProperty.all(ContinuousRectangleBorder(borderRadius: BorderRadius.circular(10))),
+      elevation: MaterialStateProperty.all(5),
+      backgroundColor: MaterialStateProperty.all(Colors.deepPurple.shade200),
+      padding: MaterialStateProperty.all(EdgeInsets.all(20))
+      );
 }
